e5da877613d308445f2d1df94f2443096e0020ef
David Puerta Martos <dpuerta@usal.es>
2024-09-02 21:22:42 +0200

    transformer controllers to mach frontend expected format

--- backend_postgres/controllers/blogs.js
+++ backend_postgres/controllers/blogs.js
@@ -13,100 +13,168 @@ const getTokenFrom = request => {
 }
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.findAll({
-    include: [
-      { model: User, attributes: ['email'] },
-      { model: Comment, attributes: ['content', 'date'] }
-    ]
-  })
-  console.log(blogs)
-  response.json(blogs)
-})
-
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findByPk(request.params.id, {
-    include: [
-      { model: User, attributes: ['email'] },
-      { model: Comment, attributes: ['content', 'date', 'userId'] }
-    ]
-  })
-
-  if (blog) {
-    response.json(blog)
-  } else {
-    response.status(404).json({ error: 'blog not found' })
-  }
-})
-
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findByPk(decodedToken.id)
-  if (user.role !== 'admin') {
-    return response.status(403).json({ error: 'only admins can create blogs' })
-  }
-
-  const blog = await Blog.create({
-    title: body.title,
-    content: body.content,
-    author: body.author,
-    userId: user.id,
+    const blogs = await Blog.findAll({
+      include: [
+        { model: User, attributes: ['id', 'email'] },
+        { model: Comment, attributes: ['id', 'content', 'date'] }
+      ]
+    })
+  
+    // Transform the data to match MongoDB format
+    const transformedBlogs = blogs.map(blog => ({
+      id: blog.id, // Transform id to _id
+      title: blog.title,
+      content: blog.content,
+      author: blog.author,
+      date: blog.date,
+      user: blog.User ? { id: blog.User.id, email: blog.User.email } : null, // Nest user information
+      comments: blog.Comments.map(comment => ({
+        id: comment.id,
+        content: comment.content,
+        date: comment.date
+      }))
+    }))
+  
+    response.json(transformedBlogs)
   })
 
-  response.status(201).json(blog)
-})
+  blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findByPk(request.params.id, {
+      include: [
+        { model: User, attributes: ['id', 'email'] },
+        { model: Comment, attributes: ['id', 'content', 'date', 'userId'] }
+      ]
+    })
+  
+    if (blog) {
+      const transformedBlog = {
+        id: blog.id,
+        title: blog.title,
+        content: blog.content,
+        author: blog.author,
+        date: blog.date,
+        user: blog.User ? { id: blog.User.id, email: blog.User.email } : null,
+        comments: blog.Comments.map(comment => ({
+          id: comment.id,
+          content: comment.content,
+          date: comment.date,
+          userId: comment.userId
+        }))
+      }
+      response.json(transformedBlog)
+    } else {
+      response.status(404).json({ error: 'blog not found' })
+    }
+  })
+  
 
-blogsRouter.put('/:id', async (request, response) => {
-  const { title, content, author } = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+  
+    const user = await User.findByPk(decodedToken.id)
+    if (user.role !== 'admin') {
+      return response.status(403).json({ error: 'only admins can create blogs' })
+    }
+  
+    const blog = await Blog.create({
+      title: body.title,
+      content: body.content,
+      author: body.author,
+      userId: user.id,
+    })
+  
+    // Transform the created blog data to match MongoDB format
+    const transformedBlog = {
+      id: blog.id,
+      title: blog.title,
+      content: blog.content,
+      author: blog.author,
+      date: blog.date,
+      user: { id: user.id, email: user.email },
+      comments: []
+    }
+  
+    response.status(201).json(transformedBlog)
+  })
+  
 
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
+  blogsRouter.put('/:id', async (request, response) => {
+    const { title, content, author } = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+  
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+  
+    const user = await User.findByPk(decodedToken.id)
+    if (user.role !== 'admin') {
+      return response.status(403).json({ error: 'only admins can update blogs' })
+    }
+  
+    const blog = await Blog.findByPk(request.params.id, {
+      include: [
+        { model: User, attributes: ['id', 'email'] },
+        { model: Comment, attributes: ['id', 'content', 'date', 'userId'] }
+      ]
+    })
+  
+    if (blog) {
+      blog.title = title
+      blog.content = content
+      blog.author = author
+      await blog.save()
+  
+      // Transform the updated blog data to match MongoDB format
+      const transformedBlog = {
+        id: blog.id,
+        title: blog.title,
+        content: blog.content,
+        author: blog.author,
+        date: blog.date,
+        user: blog.User ? { id: blog.User.id, email: blog.User.email } : null,
+        comments: blog.Comments.map(comment => ({
+          id: comment.id,
+          content: comment.content,
+          date: comment.date,
+          userId: comment.userId
+        }))
+      }
+  
+      response.json(transformedBlog)
+    } else {
+      response.status(404).end()
+    }
+  })
+  
 
-  const user = await User.findByPk(decodedToken.id)
-  if (user.role !== 'admin') {
-    return response.status(403).json({ error: 'only admins can update blogs' })
-  }
-
-  const blog = await Blog.findByPk(request.params.id)
-  if (blog) {
-    blog.title = title
-    blog.content = content
-    blog.author = author
-    await blog.save()
-    response.json(blog)
-  } else {
-    response.status(404).end()
-  }
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findByPk(decodedToken.id)
-  if (user.role !== 'admin') {
-    return response.status(403).json({ error: 'only admins can delete blogs' })
-  }
-
-  const blog = await Blog.findByPk(request.params.id)
-  if (!blog) {
-    return response.status(404).json({ error: 'blog not found' })
-  }
-
-  await blog.destroy()
-  response.status(204).end()
-})
+  blogsRouter.delete('/:id', async (request, response) => {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+  
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+  
+    const user = await User.findByPk(decodedToken.id)
+    if (user.role !== 'admin') {
+      return response.status(403).json({ error: 'only admins can delete blogs' })
+    }
+  
+    const blog = await Blog.findByPk(request.params.id)
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
+    }
+  
+    await blog.destroy()
+    response.status(204).end()
+  })
+  
 
 module.exports = blogsRouter
