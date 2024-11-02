63eb40cd724a1eb293334925841eeb0f2bce1fca
David Puerta Martos <dpuerta@usal.es>
2024-11-02 11:33:06 +0100

    added google loging to postgres backend

--- backend_postgres/controllers/login.js
+++ backend_postgres/controllers/login.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const axios = require('axios')
+const config = require('../utils/config')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
@@ -28,6 +30,65 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: TOKEN_EXPIRATION })
 
   response.status(200).send({ token, email: user.email, role: user.role })
+})
+
+loginRouter.post('/google', async (req, res) => {
+  const { code, redirectUri } = req.body
+  const ALLOWED_REDIRECT_URIS = [
+    `http://${config.DOMINIO}/auth/google/callback`,
+    `https://${config.DOMINIO}/auth/google/callback`,
+  ]
+
+  // Validate the redirect URI
+  if (!ALLOWED_REDIRECT_URIS.includes(redirectUri)) {
+    return res.status(400).send({ error: 'Invalid redirect URI' })
+  }
+
+  try {
+    // Create a new URLSearchParams object for form data
+    const params = new URLSearchParams({
+      code: code,
+      client_id: config.GOOGLE_CLIENT_ID,
+      client_secret: config.GOOGLE_CLIENT_SECRET,
+      redirect_uri: redirectUri,
+      grant_type: 'authorization_code',
+    })
+
+    // Exchange authorization code for tokens
+    const tokenResponse = await axios.post(
+      'https://oauth2.googleapis.com/token',
+      params.toString(),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+
+    const { id_token } = tokenResponse.data
+    // Decode ID token to get user info
+    const userInfo = jwt.decode(id_token)
+    const { email } = userInfo
+    console.log('User info received from Google:', email)
+
+    // Find or create user in your database
+    let user = await User.findOne({ where: { email } })
+    if (!user) {
+      user = await User.create({ email })
+    }
+
+    // Generate your own JWT token
+    const userForToken = { email: user.email, id: user.id }
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res.status(200).send({ token, email: user.email })
+  } catch (error) {
+    console.error(
+      'Error during Google login:',
+      error.response ? error.response.data : error.message
+    )
+    res.status(401).send({ error: 'Invalid authorization code' })
+  }
 })
 
 module.exports = loginRouter
--- backend_postgres/utils/config.js
+++ backend_postgres/utils/config.js
@@ -7,10 +7,16 @@ const POSTGRES_URI = process.env.NODE_ENV === 'development'
 
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
+const DOMINIO = process.env.DOMINIO ? process.env.DOMINIO : 'localhost:5173'
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
 module.exports = {
   POSTGRES_URI,
   PORT,
   ADMIN_EMAIL,
-  ADMIN_PASSWORD
+  ADMIN_PASSWORD,
+  DOMINIO,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
 }
